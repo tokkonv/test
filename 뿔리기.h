a2fda1617f3699836687716ddc719e5a3c27b9a2
Changhoon Ryu <tokkonv@gmail.com>
2017-03-01 17:38:46 +0900

    테스트로 올려봄

---
+++ 뿔리기.h
@@ -0,0 +1,15 @@
+for (int x = 1; x < 46; x++)
+{
+	for (int y=0; y<45; y++)
+	{
+		for(int z = 0; z < 15; z++)
+		{
+			int a = 15 - z;
+			for(int w = 0; w < a; w++)
+			{
+				roulletA[y] = lotto[x];
+			}
+			
+		}
+	}
+}
